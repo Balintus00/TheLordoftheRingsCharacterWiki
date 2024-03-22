97d067c50c82104ec6b554336e8d1b370229b2c2
Balintus00 <traxlerbalint@gmail.com>
2024-03-22 11:48:23 +0100

    Create project
    - Also added custom icon

---
+++ app/build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.compose.multiplatform)
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = libs.versions.java.get()
+            }
+        }
+    }
+
+    sourceSets {
+        val androidMain by getting {
+            dependencies {
+                implementation(libs.compose.jetpack.preview)
+                implementation(libs.androidx.activity.compose)
+            }
+        }
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.material3)
+                implementation(compose.ui)
+                implementation(compose.components.resources)
+                implementation(compose.components.uiToolingPreview)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "hu.bme.aut.ixnoyb.thelordoftheringscharacterwiki"
+    compileSdk = libs.versions.android.sdk.compile.get().toInt()
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
+    defaultConfig {
+        applicationId = "hu.bme.aut.ixnoyb.thelordoftheringscharacterwiki"
+        minSdk = libs.versions.android.sdk.min.get().toInt()
+        targetSdk = libs.versions.android.sdk.compile.get().toInt()
+        versionCode = 1
+        versionName = libs.versions.theLordOfTheRingsCharacterWiki.get()
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        val javaVersionEnumName = "VERSION_${libs.versions.java.get()}"
+
+        sourceCompatibility = JavaVersion.valueOf(javaVersionEnumName)
+        targetCompatibility = JavaVersion.valueOf(javaVersionEnumName)
+    }
+    dependencies {
+        debugImplementation(libs.compose.jetpack.tooling)
+    }
+}
---
+++ app/src/androidMain/kotlin/hu/bme/aut/ixnoyb/thelordoftheringscharacterwiki/ui/theme/Theme.android.kt
@@ -0,0 +1,13 @@
+package hu.bme.aut.ixnoyb.thelordoftheringscharacterwiki.ui.theme
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.runtime.Composable
+
+/*
+@Composable
+actual fun ApplyPlatformSpecificThemeSettings(
+    colorScheme: ColorScheme,
+    isDarkTheme: Boolean,
+) {
+    // TODO
+}*/
