6daf0c0e32f099d6ed0ee538df1d642213a9ef37
Balintus00 <traxlerbalint@gmail.com>
2024-07-07 19:44:37 +0200

    Add detekt to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.buildkonfig)
     alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.detekt)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
     alias(libs.plugins.sqldelight)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,27 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.buildkonfig) apply false
     alias(libs.plugins.compose.multiplatform) apply false
+    alias(libs.plugins.detekt)
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.sqldelight) apply false
+}
+
+// https://github.com/detekt/detekt/issues/3663#issuecomment-999866611
+allprojects {
+    tasks.register("detektAll") {
+        group = LifecycleBasePlugin.VERIFICATION_GROUP
+        dependsOn(tasks.withType<Detekt>())
+    }
+
+    tasks.configureEach {
+        if (name == LifecycleBasePlugin.BUILD_TASK_NAME) {
+            dependsOn(tasks.withType<Detekt>())
+        }
+    }
 }
